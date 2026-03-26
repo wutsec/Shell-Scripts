f9196e56b58e7ed9662a179c667b2a069151e729
delorean <105084806+wutsec@users.noreply.github.com>
2026-03-26 15:08:26 -0400

    netupdate.sh

--- netupdate.sh
+++ netupdate.sh
@@ -7,8 +7,30 @@ echo "Network-Wide Linux Update"
 sleep 0.5
 echo "(c) Devon M. 2025"
 sleep 0.5
+
+# Prompt
+echo ""
+read -p "Would you like to continue? (y/n)" menu_input
+
+case $menu_input in
+	n ) echo "Terminating script..."; sleep 0.5; exec "/home/devon/Bash/hud.sh";;
+	y ) echo "Executing mass update scripts...";;
+	* ) echo "Invalid response"; exec "/home/devon/Bash/netupdate.sh";;
+esac
+
+# Update Local Device
 sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean
+
+# Update Raspberry Pi 1
+clear
+echo "Updating Raspberry Pi 1"
+sleep 0.5
 ssh devon@192.168.1.5 'sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean'
-ssh devon@192.168.1.42 'sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean'
+
+#Update Dell Poweredge T310
+clear
+echo "Updating Dell Poweredge T310"
+sleep 0.5
+ssh devon@192.168.1.19 'sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean'
 
 exec "/home/devon/Bash/hud.sh"
