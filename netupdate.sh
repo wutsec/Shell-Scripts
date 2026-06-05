8260a456a4e32baa9f5ee6d8e9fe21d5e1902515
delorean <105084806+wutsec@users.noreply.github.com>
2026-06-05 08:14:45 -0400

    Update netupdate.sh

--- netupdate.sh
+++ netupdate.sh
@@ -31,7 +31,7 @@ ssh devon@192.168.1.5 "sudo apt update && sudo apt upgrade -y && sudo apt autore
 clear
 echo "Updating Dell Poweredge T310"
 sleep 0.5
-ssh devon@192.168.1.19 "sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean"
+ssh -p 2222 devon@192.168.1.19 "sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean"
 
 #Update Dell Poweredge R210
 clear
